d29b5d9a72f4aa36a997571d68ab303ae39d4777
Matin gholizadeh <matin.gh.work@gmail.com>
2025-05-07 16:08:25 +0330

    fixedMore/LessDescription

--- BlazorSample.FoodRecipes.UI/wwwroot/js/lineClampHelper.js
+++ BlazorSample.FoodRecipes.UI/wwwroot/js/lineClampHelper.js
@@ -1,12 +1,7 @@
-﻿window.checkSingleDescription = (dotNetRef, id) => {
-    setTimeout(() => {
-        const el = document.getElementById(`desc-${id}`);
-        if (el) {
-            const lineHeight = parseFloat(getComputedStyle(el).lineHeight);
-            const maxLines = 2;
-            const maxHeight = lineHeight * maxLines;
-            const isOverflow = el.scrollHeight > maxHeight;
-            dotNetRef.invokeMethodAsync("SetShowMore", isOverflow);
-        }
-    }, 50);
-};
+﻿window.checkOverflow = (element) => {
+    const lineHeight = parseFloat(getComputedStyle(element).lineHeight);
+    const maxLines = 2;
+    const maxHeight = lineHeight * maxLines;
+    return element.scrollHeight > maxHeight;
+}
+
